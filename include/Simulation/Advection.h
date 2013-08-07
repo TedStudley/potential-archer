c458daaf8239444e97021b54338504b3351196a8
Ted Studley <TedStudley@Gmail.com>
2013-08-07 11:02:58 -0700

    Added additional shape functions and formatted files using astyle.

--- include/Simulation/Advection.h
+++ include/Simulation/Advection.h
@@ -11,59 +11,65 @@
 using namespace Eigen;
 
 // Single in-place step of upwind
-inline void upwind (Ref<VectorXd> tempVect, const double sigma) {
-	VectorXd oldVect = tempVect;
-	double fluxL, fluxR;
-	int n = tempVect.rows();
-	for(int i = 0; i < n; ++i) {
-		fluxL = sigma * oldVect[mod(i - 1, n)];
-		fluxR = sigma * oldVect[i];
-		tempVect[i] = oldVect[i] + (fluxL - fluxR);
-	}
+inline void upwind (Ref<VectorXd> tempVect, const double sigma)
+{
+  VectorXd oldVect = tempVect;
+  double fluxL, fluxR;
+  int n = tempVect.rows();
+  for(int i = 0; i < n; ++i)
+    {
+      fluxL = sigma * oldVect[mod(i - 1, n)];
+      fluxR = sigma * oldVect[i];
+      tempVect[i] = oldVect[i] + (fluxL - fluxR);
+    }
 }
 
 // Multiple in-place upwind steps
 void upwind (Ref<VectorXd>, const double, const int);
 
 // Single in-place step of Fromm's Method
-inline void frommMethod (Ref<VectorXd> tempVect, const double sigma) {
-	VectorXd oldVect = tempVect;
-	int n = tempVect.rows();
-	for(int i = 0; i < n; ++i) {
-		tempVect[i] = oldVect[i] + sigma * ((oldVect[mod(i - 1, n)] + (1.0 - sigma) * 0.25 *(oldVect[i] - oldVect[mod(i - 2, n)])) -
-											(oldVect[i] + (1.0 - sigma) * 0.25 * (oldVect[mod(i + 1, n)] - oldVect[mod(i - 1, n)])));
-	}
+inline void frommMethod (Ref<VectorXd> tempVect, const double sigma)
+{
+  VectorXd oldVect = tempVect;
+  int n = tempVect.rows();
+  for(int i = 0; i < n; ++i)
+    {
+      tempVect[i] = oldVect[i] + sigma * ((oldVect[mod(i - 1, n)] + (1.0 - sigma) * 0.25 *(oldVect[i] - oldVect[mod(i - 2, n)])) -
+                                          (oldVect[i] + (1.0 - sigma) * 0.25 * (oldVect[mod(i + 1, n)] - oldVect[mod(i - 1, n)])));
+    }
 }
 
 // Multiple in-place Fromm's Method steps
 void frommMethod (Ref<VectorXd>, const double, const int);
 
 // Single in-place step of Fromm Van-Leer
-inline void frommVanLeer (Ref<VectorXd> tempVect, const double sigma) {
-	double thetaL, thetaR, phiL, phiR, VLDeltaL, VLDeltaR, fluxL, fluxR;
-	int n = tempVect.rows();
-	VectorXd oldVect = tempVect;
-	for(int i = 0; i < n; ++i) {
-		thetaL = min3 (2.0 * abs(oldVect[mod (i - 1, n)] - oldVect[mod (i - 2, n)]),
-					   0.5 * abs(oldVect[i] - oldVect[mod (i - 2, n)]),
-					   2.0 * abs(oldVect[i] - oldVect[mod (i - 1, n)]));
-		thetaR = min3 (2.0 * abs(oldVect[i] - oldVect[mod (i - 1, n)]),
-					   0.5 * abs(oldVect[mod (i + 1, n)] - oldVect[mod (i - 1, n)]),
-                       2.0 * abs(oldVect[mod (i + 1, n)] - oldVect[i]));
+inline void frommVanLeer (Ref<VectorXd> tempVect, const double sigma)
+{
+  double thetaL, thetaR, phiL, phiR, VLDeltaL, VLDeltaR, fluxL, fluxR;
+  int n = tempVect.rows();
+  VectorXd oldVect = tempVect;
+  for(int i = 0; i < n; ++i)
+    {
+      thetaL = min3 (2.0 * abs(oldVect[mod (i - 1, n)] - oldVect[mod (i - 2, n)]),
+                     0.5 * abs(oldVect[i] - oldVect[mod (i - 2, n)]),
+                     2.0 * abs(oldVect[i] - oldVect[mod (i - 1, n)]));
+      thetaR = min3 (2.0 * abs(oldVect[i] - oldVect[mod (i - 1, n)]),
+                     0.5 * abs(oldVect[mod (i + 1, n)] - oldVect[mod (i - 1, n)]),
+                     2.0 * abs(oldVect[mod (i + 1, n)] - oldVect[i]));
 
-        phiL = (oldVect[i] - oldVect[mod (i - 1, n)]) *
-               (oldVect[mod (i - 1, n)] - oldVect[mod (i - 2, n)]);
-        phiR = (oldVect[mod (i + 1, n)] - oldVect[i]) *
-               (oldVect[i] - oldVect[mod (i - 1, n)]);
+      phiL = (oldVect[i] - oldVect[mod (i - 1, n)]) *
+             (oldVect[mod (i - 1, n)] - oldVect[mod (i - 2, n)]);
+      phiR = (oldVect[mod (i + 1, n)] - oldVect[i]) *
+             (oldVect[i] - oldVect[mod (i - 1, n)]);
 
-        VLDeltaL = (phiL > 0) ? (thetaL * sign (oldVect[i] - oldVect[mod (i - 2, n)])) : 0;
-        VLDeltaR = (phiR > 0) ? (thetaR * sign (oldVect[mod (i + 1, n)] - oldVect[mod (i - 1, n)])) : 0;
-        
-        fluxL = oldVect[mod (i - 1, n)] + 0.5 * (1.0 - sigma) * VLDeltaL;
-        fluxR = oldVect[i] + 0.5 * (1.0 - sigma) * VLDeltaR;
+      VLDeltaL = (phiL > 0) ? (thetaL * sign (oldVect[i] - oldVect[mod (i - 2, n)])) : 0;
+      VLDeltaR = (phiR > 0) ? (thetaR * sign (oldVect[mod (i + 1, n)] - oldVect[mod (i - 1, n)])) : 0;
 
-        tempVect[i] = oldVect[i] + sigma * (fluxL - fluxR);
-	}
+      fluxL = oldVect[mod (i - 1, n)] + 0.5 * (1.0 - sigma) * VLDeltaL;
+      fluxR = oldVect[i] + 0.5 * (1.0 - sigma) * VLDeltaR;
+
+      tempVect[i] = oldVect[i] + sigma * (fluxL - fluxR);
+    }
 }
 
 // Multiple in-place steps of Fromm Van-Leer
--- source/main.cpp
+++ source/main.cpp
@@ -6,27 +6,29 @@
 
 #include <iomanip>
 
-int main() {
-	int     n = GRID_SIZE;
-	double  h = 1.0 / n,
-		      sigma = SIGMA,
-		      v	= VELOCITY,
+int main()
+{
+  int     n = GRID_SIZE;
+  double  h = 1.0 / n,
+          sigma = SIGMA,
+          v	= VELOCITY,
           k = sigma * h / v,
           t = 0.0;
   int end_step    = END_TIME / k;
-	Eigen::VectorXd heatVect(n);
+  Eigen::VectorXd heatVect(n);
 
-	squareWave(heatVect);
+  squareWave(heatVect);
 
   std::cout << heatVect.transpose() << std::endl;
-  for (int i = 0; i < 5; ++i) {
-  
+  for (int i = 0; i < 5; ++i)
+    {
+
       crankNicholson(heatVect, sigma);
 //      frommVanLeer(heatVect, sigma);
       std::cout << heatVect.transpose() << std::endl;
       t += k;
-  }
+    }
 
 
-	return 0;
+  return 0;
 }
